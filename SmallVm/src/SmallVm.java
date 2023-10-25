9c688d3eabad4956c9972482a4fa7ebdaf8dbef2
AndrewTheCoder <76978271+TheFadedOne@users.noreply.github.com>
2023-10-24 22:59:19 -0400

    added the files

---
+++ SmallVm/src/SmallVm.java
@@ -0,0 +1,58 @@
+import java.io.*;
+
+public class SmallVm {
+
+	public static final int MAX_MEMORY_SIZE = 1000;
+	//public static Descriptor[] dataMemory;
+	public static String[] instructionsMemory;
+	public static boolean programIsRunning = false;
+	
+	public static void main(String[] args) {
+		
+        loadInstructionsToMemory();
+        runProgram();
+	}
+
+	/*
+	 * Loads the instructions into memory
+	 */
+	public static void loadInstructionsToMemory() {
+		
+		instructionsMemory = new String[MAX_MEMORY_SIZE];
+		File file = new File("mySmallVm_Prog.txt");
+		BufferedReader bufferedReader = null;
+		 
+		try {
+			bufferedReader = new BufferedReader(new FileReader(file));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+        String string;
+        try {
+        	//load each individual instruction to memory
+        	int counter = 0;
+			while ((string = bufferedReader.readLine()) != null) {
+				instructionsMemory[counter] = string;
+				System.out.println(instructionsMemory[counter]);
+			    counter++;
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/*
+	 * Starts the fetch, decode, execute cycle from the instruction at memory index 0
+	 * Will end upon reading HALT instruction
+	 */
+	public static void runProgram() {
+		
+		//dataMemory = new Descriptor[MAX_MEMORY_SIZE];
+		int programCounter = 0;
+		
+		while(true) {
+			
+		}
+	}
+	
+}
